3dd48dd0084109b36d6f8bcb77a6ebd34447a3f7
damar <damar@gitlab.lab>
2021-03-29 10:23:52 +0700

    test redirect lagi

--- src/api.js
+++ src/api.js
@@ -6,13 +6,13 @@ const app = express();
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('*/', (req, res) => {
     res.json({
         "hello": "hi"
     })
 });
 
-router.get('/test', (req, res) => {
+router.get('*/test', (req, res) => {
     res.json({
         "hello": "ini adalah test"
     })
